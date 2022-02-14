94a74698eb294e88f63f214b312fe38bc6d85237
Canh.Nguyen <leagueyasuo186@gmail.com>
2022-02-14 18:42:53 +0700

    l

---
+++ src/testPoint.java
@@ -0,0 +1,8 @@
+public class testPoint {
+    public static void main(String[] args) {
+        Point point = new Point();
+        System.out.println(point);
+        point = new Point(1, 1);
+        System.out.println(point);
+    }
+}
